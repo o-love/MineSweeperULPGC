b0dd90ae9105e7cddf003d445c8da317b2999c24
Oscar Love <olabradalove@gmail.com>
2022-10-09 17:25:11 +0100

    Basic Tests for MineSweeperBoardInterface

--- src/main/java/net/oscarlove/minesweeper/model/interfaces/MineSweeperBoardInterface.java
+++ src/main/java/net/oscarlove/minesweeper/model/interfaces/MineSweeperBoardInterface.java
@@ -4,6 +4,16 @@ import net.oscarlove.minesweeper.model.cell.Cell;
 
 public interface MineSweeperBoardInterface {
 
+    /**
+     *
+     *
+     *
+     * @param i row, zero counted
+     * @param j column, zero counted
+     * @return The cell found in that row and column.
+     *
+     * @throws IndexOutOfBoundsException When the index of either parameter exceeds the board's length.
+     */
     Cell getCell(int i, int j);
     int getNumberOfMines();
     int getNumberOfUnflaggedMines();
