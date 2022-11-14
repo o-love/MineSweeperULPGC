d480f82047fe26edc6978846ab35b03939e9ca46
Oscar Love <olabradalove@gmail.com>
2022-11-14 06:29:09 +0000

    OpenObservableTest test setup

--- src/main/java/net/oscarlove/minesweeper/base/observer/OpenObservable.java
+++ src/main/java/net/oscarlove/minesweeper/base/observer/OpenObservable.java
@@ -6,4 +6,25 @@ import java.util.Set;
 
 public interface OpenObservable extends Observable {
     void updateObservers();
+
+    static OpenObservable create() {
+        return new OpenObservable() {
+
+            private final Collection<Observer> observers = new HashSet<>();
+            @Override
+            public void updateObservers() {
+
+            }
+
+            @Override
+            public void addObserver(Observer observer) {
+
+            }
+
+            @Override
+            public void removeObserver(Observer observer) {
+
+            }
+        };
+    }
 }
---
+++ src/test/java/net/oscarlove/minesweeper/model/OpenObservableTest.java
@@ -0,0 +1,12 @@
+package net.oscarlove.minesweeper.model;
+
+import net.oscarlove.minesweeper.base.observer.OpenObservable;
+
+public class OpenObservableTest {
+
+
+
+    private OpenObservable build() {
+        return OpenObservable.create();
+    }
+}
