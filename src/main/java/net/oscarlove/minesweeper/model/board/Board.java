fd4ba3f1aba7325195621b7aaf6754c073a19d46
Oscar Love <olabradalove@gmail.com>
2022-11-30 15:00:01 +0000

    Board is interface

--- src/main/java/net/oscarlove/minesweeper/model/board/Board.java
+++ src/main/java/net/oscarlove/minesweeper/model/board/Board.java
@@ -4,24 +4,63 @@ import net.oscarlove.minesweeper.model.Dimension;
 import net.oscarlove.minesweeper.model.Position;
 import net.oscarlove.minesweeper.model.cell.Cell;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 
-public class Board {
+public interface Board {
+
+    Cell getCell(Position position);
+
+    int getCellValue(Position position);
+
+    Dimension produceDimension();
+
 
     /**
      * @throws NullPointerException     If any of the arguments are {@code null}.
      * @throws IllegalArgumentException If {@code cells} is not rectangular.
      */
-    public static Board create(List<List<Cell>> cells, Collection<Position> minePositions) {
+    static Board create(List<List<Cell>> cells, Collection<Position> minePositions) {
         checkArguments(cells, minePositions);
 
-        return new Board(cells, minePositions);
+        return new Board() {
+            public Cell getCell(Position position) {
+                return cells.get(position.row()).get(position.column());
+            }
+
+            public int getCellValue(Position position) {
+                if (isAMine(position)) {
+                    return -1;
+                }
+
+                return (int) minePositions.stream()
+                        .filter(minePos -> isAdjacent(minePos, position))
+                        .count();
+            }
+
+            public Dimension produceDimension() {
+                return new Dimension(cells.size(), cells.get(0).size());
+            }
+
+            private boolean isAMine(Position position) {
+                return minePositions.contains(position);
+            }
+
+            private boolean isAdjacent(Position pos1, Position pos2) {
+                return isAdjacent(pos1.row(), pos2.row()) && isAdjacent(pos1.column(), pos2.column());
+            }
+
+            private boolean isAdjacent(int int1, int int2) {
+                return Math.abs(int1 - int2) <= 1;
+            }
+        };
     }
 
     /**
      * @throws NullPointerException If {@code boardCellGenerator} is {@code null}.
      */
-    public static Board create(BoardCellGenerator boardCellGenerator) {
+    static Board create(BoardCellGenerator boardCellGenerator) {
         Objects.requireNonNull(boardCellGenerator, "boardCellGenerator can not be null.");
 
         return create(boardCellGenerator.get(), boardCellGenerator.getLastGenerationMinedCellPositions());
@@ -38,68 +77,5 @@ public class Board {
 
     private static boolean isAllRowsSameLength(List<List<Cell>> cells) {
         return cells.stream().allMatch(list -> cells.get(0).size() == list.size());
-    }
-
-    private final List<List<Cell>> cells;
-    private final Collection<Position> minePositions;
-
-    private Board(List<List<Cell>> cells, Collection<Position> minePositions) {
-        this.cells = copy(cells);
-        this.minePositions = copy(minePositions);
-    }
-
-    public Cell getCell(Position position) {
-        return cells.get(position.row()).get(position.column());
-    }
-
-    public int getCellValue(Position position) {
-        if (isAMine(position)) {
-            return -1;
-        }
-
-        return (int) minePositions.stream()
-                .filter(minePos -> isAdjacent(minePos, position))
-                .count();
-    }
-
-    public Dimension produceDimension() {
-        return new Dimension(cells.size(), cells.get(0).size());
-    }
-
-    private boolean isAMine(Position position) {
-        return minePositions.contains(position);
-    }
-
-    private boolean isAdjacent(Position pos1, Position pos2) {
-        return isAdjacent(pos1.row(), pos2.row()) && isAdjacent(pos1.column(), pos2.column());
-    }
-
-    private boolean isAdjacent(int int1, int int2) {
-        return Math.abs(int1 - int2) <= 1;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Board baseBoard = (Board) o;
-        return cells.equals(baseBoard.cells) && minePositions.equals(baseBoard.minePositions);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(cells, minePositions);
-    }
-
-    // Constructor helper functions
-
-    private List<List<Cell>> copy(List<List<Cell>> cells) {
-        return new ArrayList<>(
-                cells.stream().map(ArrayList::new).toList()
-        );
-    }
-
-    private Collection<Position> copy(Collection<Position> minePositions) {
-        return new HashSet<>(minePositions);
     }
 }
