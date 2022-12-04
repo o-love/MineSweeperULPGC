fec60e5d186c4a2b98d63379121bdc5eb49a270b
Oscar Love <olabradalove@gmail.com>
2022-12-04 13:44:09 +0000

    Final Commit Before Refactor

--- src/main/java/net/oscarlove/minesweeper/view/swing/GUISwing.java
+++ src/main/java/net/oscarlove/minesweeper/view/swing/GUISwing.java
@@ -2,25 +2,22 @@ package net.oscarlove.minesweeper.view.swing;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 public class GUISwing extends JFrame {
-    private static class WindowCloseManager extends WindowAdapter {
-        @Override
-        public void windowClosing(WindowEvent windowEvent) {
-            System.exit(0);
-        }
-    }
+
 
     public GUISwing(JPanel jPanel, String string, Dimension dimension) {
+        this(jPanel, string, dimension, JFrame.EXIT_ON_CLOSE);
+    }
+
+    public GUISwing(JPanel jPanel, String string, Dimension dimension, int defaultCloseOperation) {
         super(string);
 
         getContentPane().add(jPanel);
 
         setSize(dimension.width, dimension.height);
 
-        this.addWindowListener(new WindowCloseManager());
-        setVisible(true);
+        this.setDefaultCloseOperation(defaultCloseOperation);
+        this.setVisible(true);
     }
 }
